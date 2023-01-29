cde9623304dfc34f828c46a802f54d18a82e4911
tauBootist <taubootist@gmail.com>
2023-01-29 00:58:23 -0500

    misc ui

--- rube-react/src/App.tsx
+++ rube-react/src/App.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import "./App.css";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
 
 function App() {
+  const Item = styled(Button)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+  }));
+
   const onClick = () => {
     alert("Nice to see you!");
   };
 
   return (
-    <div className="App">
-      <Button variant="outlined" onClick={onClick}>
-        Hello World
-      </Button>
-    </div>
+    <Box sx={{ width: "100%" }}>
+      <Stack spacing={2}>
+        <Stack direction="row" spacing={2}>
+          <Item variant="outlined" onClick={onClick}>
+            Hello World
+          </Item>
+        </Stack>
+      </Stack>
+    </Box>
   );
 }
 
